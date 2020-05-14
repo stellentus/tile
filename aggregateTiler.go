215714e8d757d3d62aaff53e63420880352e830b
James Bell <james@bellstone.ca>
2020-05-13 20:34:22 -0600

    Add single and pair tilers

--- aggregateTiler.go
+++ aggregateTiler.go
@@ -24,3 +24,58 @@ func (til *aggregateTiler) Tile(data []float64) []uint64 {
 
 	return output
 }
+
+// singleTiler is used for tile coding when multiple Tilers must work together.
+type singleTiler struct {
+	idx int
+	til Tiler
+}
+
+func (til *singleTiler) Tile(data []float64) []uint64 {
+	return til.Tile([]float64{data[til.idx]})
+}
+
+// NewSinglesTiler creates a new Tiler which tiles each dimension individually.
+func NewSinglesTiler(numDims, tiles int) (Tiler, error) {
+	tils := make([]Tiler, numDims)
+	for i := range tils {
+		til, err := NewHashTiler(tiles)
+		if err != nil {
+			return nil, err
+		}
+		tils[i] = &singleTiler{
+			idx: i,
+			til: til,
+		}
+	}
+	return NewAggregateTiler(tils)
+}
+
+// pairTiler is used for tile coding when multiple Tilers must work together.
+type pairTiler struct {
+	idx1, idx2 int
+	til        Tiler
+}
+
+func (til *pairTiler) Tile(data []float64) []uint64 {
+	return til.Tile([]float64{data[til.idx1], data[til.idx2]})
+}
+
+// NewPairsTiler creates a new Tiler which tiles each pair of dimensions.
+func NewPairsTiler(numDims, tiles int) (Tiler, error) {
+	tils := make([]Tiler, numDims)
+	for i := 0; i < numDims; i++ {
+		for j := i + 1; j < numDims; j++ {
+			til, err := NewHashTiler(tiles)
+			if err != nil {
+				return nil, err
+			}
+			tils[i] = &pairTiler{
+				idx1: i,
+				idx2: j,
+				til:  til,
+			}
+		}
+	}
+	return NewAggregateTiler(tils)
+}
