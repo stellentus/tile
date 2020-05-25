5d267daa75703b1c04d615b7d7b9856fe8dacf97
Matthias Stone <matthias@bellstone.ca>
2020-05-25 13:02:06 -0600

    Expose aggregate tilers

--- aggregateTiler.go
+++ aggregateTiler.go
@@ -1,7 +1,7 @@
 package tile
 
-// aggregateTiler is used for tile coding when multiple Tilers must work together.
-type aggregateTiler struct {
+// AggregateTiler is used for tile coding when multiple Tilers must work together.
+type AggregateTiler struct {
 	// tils are the underlying Tilers that generate the hashes.
 	tils []Tiler
 }
@@ -9,13 +9,13 @@ type aggregateTiler struct {
 // NewAggregateTiler creates a new Tiler which returns all of the hashes provided
 // by the individual Tilers.
 func NewAggregateTiler(tils []Tiler) (Tiler, error) {
-	return &aggregateTiler{
+	return &AggregateTiler{
 		tils: tils,
 	}, nil
 }
 
 // Tile returns a vector of indices describing the input data.
-func (til *aggregateTiler) Tile(data []float64) []uint64 {
+func (til *AggregateTiler) Tile(data []float64) []uint64 {
 	output := []uint64{}
 
 	for _, til := range til.tils {
